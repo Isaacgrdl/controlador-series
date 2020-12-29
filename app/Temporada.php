b975a75dec8b7aa84fe7f65e3d711f46de146e14
Isaac Lopes <isaacgrdlopes@gmail.com>
2020-12-29 16:49:17 -0300

    [29-12-2020] - Incluido os Models de Temporadas e Series e gerado as Migrations.

--- app/Temporada.php
+++ app/Temporada.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Temporada extends Model
 {
-    //
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class);
+    }
 }
